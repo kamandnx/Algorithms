b2e5dcaf6247c8e8731aa33f659580d476ccbf12
kamandnx <58206239+kamandnx@users.noreply.github.com>
2022-12-01 13:50:13 +0330

    Initialize Algorithm Rep

---
+++ Hackerrank/part2.py
@@ -0,0 +1,7 @@
+def insertionSort2(n, arr):
+    for i in range(1, n):
+        j=i
+        while arr[j-1]>arr[j] and j > 0:
+            arr[j-1], arr[j] = arr[j] , arr[j-1]
+            j-=1
+        print(*arr)
